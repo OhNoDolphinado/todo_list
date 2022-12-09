55e1d8fac562ecc79bd146f01317a45b84fe6859
OhNoDolphinado <aclass@sonic.net>
2022-12-09 23:01:49 +0000

    it fuckin works

--- TodoList.java
+++ TodoList.java
@@ -2,27 +2,107 @@
 // remove items from list
 // print list
 // bonus: add items to list, multiple at a time
-// bonus: sort items alphabetically
+// bonus: sort items alphabetically and by length
 
 import java.util.*;
 
 public class TodoList {
     ArrayList<String> items = new ArrayList<String>();
 
+    public void printItems() {
+        System.out.println("\nTodo List\n- - - -"); // title and formatting
+        for (int i=0; i < items.size(); i++){ // iterates through list
+            System.out.println((i+1) + ") " + items.get(i));
+        }
+    }
+
+    public void sortList(String type) {
+        if (type.equals("abc")) {
+            Collections.sort(items, new AtoZ());
+        }
+        else if (type.equals("xyz")) {
+            Collections.sort(items, new ZtoA());
+        }
+        else if (type.equals("long")) {
+            Collections.sort(items, new LongestFirst());
+        }
+        else if (type.equals("short")) {
+            Collections.sort(items, new ShortestFirst());
+        }
+        else {
+            System.out.println("Unknown sort type.");
+        }
+    }
+
     public void addItems(String item) { // adds items to list
-        System.out.println("Please type the new task below:");
         items.add(item);
     }
 
     public void removeItems(int item) { // removes items from list 
-        System.out.println("Please state the number of the task you'd like to remove:");
-        items.remove(item - 1); 
+        if (items.size() >= 1){
+            items.remove(item - 1); 
+        } 
+        else{
+            System.out.println("Your list is empty!");
+        }
     }
+    
+}
 
-    public void printItems() {
-        System.out.println("Todo List\n - - - - -"); // title and formatting
-        for (int i=0; i < items.size(); i++){ // iterates through list
-            System.out.println((i+1) + ") " + items.get(i) + "\n");
+// comparators below, for sortList():
+
+class AtoZ implements Comparator<String>{
+    public int compare(String first, String second) {
+        if (first.equals(second)){ // if they are the same, it doesn't matter
+            return 0;
+        }
+        else if (first.compareToIgnoreCase(second) > second.compareToIgnoreCase(first)) { // first is alphabetically ahead; it goes higher
+            return 1;
+        }
+        else {
+            return -1; // doesn't pass; it goes lower
+        }
+    }
+}
+
+class ZtoA implements Comparator<String>{
+    public int compare(String first, String second) {
+        if (first.equals(second)){ // if they are the same, it doesn't matter
+            return 0;
+        }
+        else if (first.compareToIgnoreCase(second) < second.compareToIgnoreCase(first)) { // first is alphabetically behind; it goes higher
+            return 1;
+        }
+        else {
+            return -1; // doesn't pass; it goes lower
+        }
+    }
+}
+
+class LongestFirst implements Comparator<String>{
+    public int compare(String first, String second) {
+        if (first.equals(second)){ // if they are the same, it doesn't matter
+            return 0;
+        }
+        else if (first.length() < second.length()) { // first is longer; its ranked better
+            return 1;
+        }
+        else {
+            return -1; // doesn't pass; it goes lower
+        }
+    }
+}
+
+class ShortestFirst implements Comparator<String>{
+    public int compare(String first, String second) {
+        if (first.equals(second)){ // if they are the same, it doesn't matter
+            return 0;
+        }
+        else if (first.length() > second.length()) { // first is shorter; its ranked better
+            return 1;
+        }
+        else {
+            return -1; // doesn't pass; it goes lower
         }
     }
 }
--- TodoListMain.java
+++ TodoListMain.java
@@ -2,32 +2,46 @@ import java.util.*;
 
 public class TodoListMain {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+
+        Scanner in = new Scanner(System.in); // intialize java.util.Scanner object
 
         TodoList list = new TodoList();
-        list.items.add("This is an example task.");
+        list.items.add("This is an example task."); // for testing
+        list.items.add("Alphabet soup is wonderful."); // for testing
+        list.items.add("password:password is a hit or miss band, but I like 'Every Single Night'."); // for testing
+        list.items.add("Zebras rule, dude."); // for testing
+        list.items.add("San Francisco is the city by the bay."); // for testing
 
         boolean exit = false;
 
-        while (exit != true) {
-            System.out.println("To see the list, type 'print'. To add an item, type 'add'. To remove an item, type 'delete'.");
-            if (in.nextLine() == "print") {
+        while (exit != true) { // decision tree
+            System.out.println("\nTo see the list, type 'print'. To sort the list, type 'sort'. To add an item, type 'add'. To remove an item, type 'delete'. To exit, type 'exit'.");
+            String input = in.nextLine(); // answers prompt above
+            
+            if (input.equals("print")) {
                 list.printItems();
-                System.out.println("print");
             }
-            else if (in.nextLine() == "add") {
+            else if (input.equals("sort")) {
+                System.out.println("To sort A-Z, type 'abc'. To sort Z-A, type 'xyz'. To sort longest to shortest, type 'long'. To sort shortest to longest, type 'short'.");
+                list.sortList(in.nextLine());
+            }
+            else if (input.equals("add")) {
+                System.out.println("Please type the new task below:");
                 list.addItems(in.nextLine());
-                System.out.println("add");
             }
-            else if (in.nextLine() == "delete") {
-                list.removeItems(in.nextInt());
-                System.out.println("delete");
+            else if (input.equals("delete")) {
+                System.out.println("Please state the number of the task you'd like to remove:");
+                list.removeItems(Integer.parseInt(in.nextLine())); // alternatively could use in.nextInt() but this is more convenient
+            }
+            else if (input.equals("exit")) {
+                System.out.println("Exiting...");
+                exit = true; // quite literally, the exit condition
             }
             else {
-                System.out.println("exit");
-                exit = true;
+                System.out.println("Invalid command. Please try again.");
             }
         }
+        // list.printItems();
         in.close();
     }
 }
